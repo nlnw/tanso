dc56618fb5116d2bea020a926ac6d965d3b68d71
nlnw <108184497+nlnw@users.noreply.github.com>
2023-06-24 18:00:29 +0000

    Integrate Ethereum Attestation Service.

--- my-app/src/components/CarbonTable.jsx
+++ my-app/src/components/CarbonTable.jsx
@@ -1,4 +1,13 @@
+import { eas } from "../utils/eas.js";
+
 export default function CarbonTable() {
+  const do_it = async () => {
+    const uid =
+      "0x68ed2e7457f20f65b2bf26190a8d88e2711e79cec945ad934e0f28a769e9ded8";
+    const attestation = await eas.getAttestation(uid);
+    console.log(attestation);
+  };
+
   return (
     <>
       <div>
@@ -27,7 +36,7 @@ export default function CarbonTable() {
 
                       <a
                         className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-                        href="#"
+                        onClick={do_it}
                       >
                         <svg
                           className="w-3 h-3"
@@ -36,7 +45,6 @@ export default function CarbonTable() {
                           height="16"
                           viewBox="0 0 16 16"
                           fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
                             d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2"

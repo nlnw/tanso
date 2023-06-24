a3af2b86e0c27d676aba971da916f0f73b5d87fe
nlnw <108184497+nlnw@users.noreply.github.com>
2023-06-24 16:29:59 +0000

    Initial app frame.

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Tanso from "./routes/Tanso.jsx";
+import ErrorPage from "./routes/ErrorPage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/tanso/",
+    element: <Tanso />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

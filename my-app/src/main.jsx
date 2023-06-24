0ab36e2e897587de04db3f868b7ba23ea4b67bbd
nlnw <108184497+nlnw@users.noreply.github.com>
2023-06-24 16:46:25 +0000

    Switch to hash router.

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,22 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Tanso from "./routes/Tanso.jsx";
 import ErrorPage from "./routes/ErrorPage.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/tanso/",
-      element: <Tanso />,
-    },
-  ],
-  { basename: "/tanso" }
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/tanso/",
+    element: <Tanso />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

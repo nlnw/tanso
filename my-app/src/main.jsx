aa00d8d58b36752e8ac970d0cbdb5d80cdd4563e
nlnw <108184497+nlnw@users.noreply.github.com>
2023-06-25 04:39:18 +0000

    Add XMTP.

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -5,6 +5,7 @@ import Tanso from "./routes/Tanso.jsx";
 import ErrorPage from "./routes/ErrorPage.jsx";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+import { XMTPProvider } from "@xmtp/react-sdk";
 
 import "@rainbow-me/rainbowkit/styles.css";
 
@@ -56,7 +57,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider value={client}>
       <WagmiConfig config={wagmiConfig}>
         <RainbowKitProvider chains={chains} theme={darkTheme()}>
-          <RouterProvider router={router} />
+          <XMTPProvider>
+            <RouterProvider router={router} />
+          </XMTPProvider>
         </RainbowKitProvider>
       </WagmiConfig>
     </Provider>

cdca605998fde948c577df20aa69140c6e0ac523
nlnw <108184497+nlnw@users.noreply.github.com>
2023-06-25 03:53:45 +0000

    Initial attestation.

--- my-app/src/components/CarbonList.jsx
+++ my-app/src/components/CarbonList.jsx
@@ -45,6 +45,35 @@ export default function CarbonList() {
   const schemaColumns = schemaEncoder.decodeData(data.attestations[0].data);
   console.log(schemaColumns);
 
+  const attestIt = async () => {
+    const eas = new EAS(EASContractAddress);
+    eas.connect(signer);
+
+    // Initialize SchemaEncoder with the schema string
+    const schemaEncoder = new SchemaEncoder("uint256 eventId, uint8 voteIndex");
+    const encodedData = schemaEncoder.encodeData([
+      { name: "eventId", value: 1, type: "uint256" },
+      { name: "voteIndex", value: 1, type: "uint8" },
+    ]);
+
+    const schemaUID =
+      "0xb16fa048b0d597f5a821747eba64efa4762ee5143e9a80600d0005386edfc995";
+
+    const tx = await eas.attest({
+      schema: schemaUID,
+      data: {
+        recipient: "0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165",
+        expirationTime: 0,
+        revocable: true,
+        data: encodedData,
+      },
+    });
+
+    const newAttestationUID = await tx.wait();
+
+    console.log("New attestation UID:", newAttestationUID);
+  };
+
   return (
     <>
       <thead className="bg-gray-50 dark:bg-slate-900">
@@ -69,6 +98,7 @@ export default function CarbonList() {
               </div>
             </th>
           ))}
+          <th scope="col" className="px-6 py-3 text-right"></th>
           <th scope="col" className="px-6 py-3 text-right"></th>
         </tr>
       </thead>
@@ -133,6 +163,111 @@ export default function CarbonList() {
             </tr>
           );
         })}
+        <tr
+          className="bg-white hover:bg-gray-50 dark:bg-slate-900 dark:hover:bg-slate-800"
+          key="new"
+        >
+          <td className="h-px w-px whitespace-nowrap">
+            <a
+              className="block"
+              data-hs-overlay="#hs-ai-invoice-modal"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="px-6 py-2">
+                <span className="font-mono text-sm text-blue-600 dark:text-blue-500"></span>
+              </div>
+            </a>
+          </td>
+          <td className="h-px w-px whitespace-nowrap">
+            <a className="block" data-hs-overlay="#hs-ai-invoice-modal">
+              <div className="px-6 py-2">
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  Solar
+                </span>
+              </div>
+            </a>
+          </td>
+          <td className="h-px w-px whitespace-nowrap">
+            <a className="block" data-hs-overlay="#hs-ai-invoice-modal">
+              <div className="px-6 py-2">
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  2222
+                </span>
+              </div>
+            </a>
+          </td>
+          <td className="h-px w-px whitespace-nowrap">
+            <a className="block" data-hs-overlay="#hs-ai-invoice-modal">
+              <div className="px-6 py-2">
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  422
+                </span>
+              </div>
+            </a>
+          </td>
+          <td className="h-px w-px whitespace-nowrap">
+            <a className="block" data-hs-overlay="#hs-ai-invoice-modal">
+              <div className="px-6 py-2">
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  645
+                </span>
+              </div>
+            </a>
+          </td>
+          <td className="h-px w-px whitespace-nowrap">
+            <a className="block" data-hs-overlay="#hs-ai-invoice-modal">
+              <div className="px-6 py-2">
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  Germany
+                </span>
+              </div>
+            </a>
+          </td>
+          <td className="h-px w-px whitespace-nowrap">
+            <a className="block" data-hs-overlay="#hs-ai-invoice-modal">
+              <div className="px-6 py-2">
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  Bavaria
+                </span>
+              </div>
+            </a>
+          </td>
+          <td className="h-px w-px whitespace-nowrap">
+            <a className="block" data-hs-overlay="#hs-ai-invoice-modal">
+              <div className="px-6 py-2">
+                <span className="text-sm text-gray-600 dark:text-gray-400">
+                  Friedberg
+                </span>
+              </div>
+            </a>
+          </td>
+
+          <td className="h-px w-px whitespace-nowrap">
+            <a
+              className="block"
+              href="javascript:;"
+              onClick={attestIt}
+              data-hs-overlay="#hs-ai-invoice-modal"
+            >
+              <div className="px-6 py-2">
+                <span className="inline-flex items-center gap-1.5 py-0.5 px-2 rounded-full text-xs font-medium bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200">
+                  <svg
+                    className="w-2.5 h-2.5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                  </svg>
+                  Not Attested
+                </span>
+              </div>
+            </a>
+          </td>
+        </tr>
       </tbody>
     </>
   );
